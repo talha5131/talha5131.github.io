b9793a1d8186b19f8df69a4b9915c95964a5ca87
Talha Safdar <talha.malik@cloudnext.tech>
2022-07-24 06:59:28 +0500

    Almost Final

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,24 @@
-function myFunction() {
-    console.log();
+function openModal(x) {
+    switch (x) {
+        case 1:
+            $('#myModal-1').modal('show');
+            break;
+        case 2:
+            $('#myModal-2').modal('show');
+            break;
+        case 3:
+            $('#myModal-3').modal('show');
+            break;
+        case 4:
+            $('#myModal-4').modal('show');
+            break;
+        case 5:
+            $('#myModal-5').modal('show');
+            break;
+        case 6:
+            $('#myModal-6').modal('show');
+            break;
+        default:
+            // Do Nothing
+    }
 }
